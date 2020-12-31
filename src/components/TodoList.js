b5fddc62b4fe4842b6292ecfb48fe89bc38c4335
Memet <meavci@gmail.com>
2020-12-30 23:00:13 -0500

    Create setState function

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -18,18 +18,38 @@ class TodoList extends React.Component{
       newTask:''
     }
     this.handleInput=this.handleInput.bind(this)
+    this.handleClick=this.handleClick.bind(this)
   }
   handleInput(e){
   this.setState({
     newTask:e.target.value
   })
   }
+  handleClick(e){
+    if(this.state.newTask.trim()){
+      let newItem= {title:this.state.newTask,
+      completed:false,
+      id:Date.now()
+    }
+    this.setState(prevState=>{
+      return {
+        tasks:prevState.tasks.concat(newItem)
+      }
+    })
+    this.state.newTask="";
+    }
+    else{
+      alert('Please enter  a value')
+    }
+   
+  }
   render(){
     return (
       <div>
      <form> 
-     <input type="text" onInput={this.handleInput}/>
-     <button type='button'>Add</button>
+     <input type="text" onInput={this.handleInput}
+     value={this.state.newTask}/>
+     <button type='button' onClick={this.handleClick}>Add</button>
      </form>
      <ul>
      <ToDoItems tasks={this.state.tasks}/>
