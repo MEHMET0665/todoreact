8e222d4f9879961f81f4d74df2c6239275f2bcd8
Memet <meavci@gmail.com>
2021-01-06 23:07:51 -0500

    remove item from DOM

--- src/components/TodoItems.js
+++ src/components/TodoItems.js
@@ -1,9 +1,17 @@
-function ToDoItems(props){
-  return props.tasks.map(item=>{
-      return <li>{
-        item.title}
-        <span className='remove-item'>(Remove)</span>
-        </li>
-    }) ;
+function TodoItems (props) {
+
+  function handleRemove(e) {
+    props.removeItem(e.target.id)
   }
-export default ToDoItems;
+
+  return props.tasks.map(item => {
+    return (
+      <li key={item.id} >
+        {item.title} &nbsp;
+        <span id={item.id} className="remove-item" onClick={handleRemove}>(Remove)</span>
+      </li>
+    )
+  });
+}
+
+export default TodoItems;
