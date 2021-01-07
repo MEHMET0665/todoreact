733760960379078f71dddb8537eb8760674ce47a
Memet <meavci@gmail.com>
2021-01-06 22:37:43 -0500

    fetch database

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,72 +1,74 @@
-import React from "react";
-import ToDoItems from "./TodoItems";
+import React from 'react';
+import TodoItems from './TodoItems';
 
-class TodoList extends React.Component{
-  constructor(props){
+class TodoList extends React.Component {
+  constructor(props) {
     super(props);
-    this.state={
-      tasks:[
-        {title:'Get a shopping cart',
-        completed:false,
-        id:12336544
-      },
-      {title:'Buy milk',
-        completed:false,
-        id:12336512
-      }
-      ],
-      newTask:''
+    this.state = {
+      tasks: [],
+      newTask: ''
     }
-    this.handleInput=this.handleInput.bind(this)
-    this.handleClick=this.handleClick.bind(this)
+    this.handleInput = this.handleInput.bind(this);
+    this.handleClick = this.handleClick.bind(this);
   }
-  handleInput(e){
-  this.setState({
-    newTask:e.target.value
-  })
-  }
-  handleClick(e){
-    if(this.state.newTask.trim()){
-      //create a new tasks onject
-      let newItem= {title:this.state.newTask,
-      completed:false,
-      id:Date.now()
-    }
-    //concatenate new task object to the previous task
-    // this.setState(prevState=>{
-    //   return {
-    //     tasks:prevState.tasks.concat(newItem)
-    //   }
-    // })
-//another concatenating method
-const newTasks=[...this.state.tasks,newItem]
-this.setState({
-  tasks:newTasks
-})
-console.log(...this.state.tasks)
 
-    // emty the newTasks property in state
-    this.state.newTask="";
-    }
-    else{
-      alert('Please enter  a value')
-    }
-   
+  handleInput(e) {
+    this.setState({
+      newTask: e.target.value
+    })
   }
-  render(){
+
+  handleClick(e) {
+    if(this.state.newTask.trim()){
+      // Create a new task object
+      let newItem = {
+        title: this.state.newTask,
+        completed: false,
+        id: Date.now()
+      }
+      // Concatenate new task object to the previous tasks in the state
+      // this.setState(prevState => {
+      //   return {
+      //     tasks: prevState.tasks.concat(newItem)
+      //   }
+      // })
+      // Another concatenating method you might fiind easier
+      const newTasks = [...this.state.tasks, newItem] 
+      this.setState({
+        tasks: newTasks
+      })
+      // Empty the newTask property in the state
+      this.state.newTask = "";
+    } else {
+      alert('Please enter a value')
+    }
+  }
+
+  componentDidMount() {
+    // fetch('http://localhost:8080/api/todoitems')
+    // .then((response) => response.json())
+    // .then((json) => this.setState({tasks: json}));
+    alert('component mounted')
+  }
+  componentDidUpdate(){
+
+  }
+  componentWillUnmount(){
+    alert('tasks added')
+  }
+  render() {
     return (
       <div>
-     <form> 
-     <input type="text" onInput={this.handleInput}
-     value={this.state.newTask}/>
-     <button type='button' onClick={this.handleClick}>Add</button>
-     </form>
-     <ul>
-     <ToDoItems tasks={this.state.tasks}/>
-     </ul>
-   
+        <form>
+          <input type="text" onInput={this.handleInput} value={this.state.newTask}/>
+          <button type="button" onClick={this.handleClick}>Add</button>
+        </form>
+        <ul>
+          <TodoItems tasks={this.state.tasks} foo="bar"/>
+        </ul>
       </div>
-
-    )}
+    )
+  }
 }
+
 export default TodoList;
